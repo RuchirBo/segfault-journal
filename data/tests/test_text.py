5dc48f3ac978185ab20042de08c74f63deffd9a9
raiyahaque <raiya811@gmail.com>
2024-10-17 17:15:06 -0400

    added test_create function for text

--- data/tests/test_text.py
+++ data/tests/test_text.py
@@ -1,5 +1,9 @@
 import data.text as tx
 
+ADD_KEY = "john.smith@nyu.edu"
+ADD_TITLE = "Journal Entry"
+ADD_TEXT = "This is my new journal entry."
+
 def test_read():
     text_data = tx.read()
     assert isinstance(text_data, dict)
@@ -25,4 +29,12 @@ def test_read_one_parameters():
 def test_read_one_invalid():
     text_data = tx.read_one("")
     assert text_data == {}
+
+
+def test_create():
+    key = tx.create(ADD_TITLE, ADD_TEXT, ADD_KEY)
+    text_data = tx.read()
+    assert key in text_data
+    assert text_data[key]['title'] == ADD_TITLE
+    assert text_data[key]['text'] == ADD_TEXT
 
