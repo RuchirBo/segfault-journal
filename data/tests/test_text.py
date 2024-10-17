df60ef1bc83c34b3ca8d0afe5bcb915b2ef0a293
castrel17 <eac9917@nyu.edu>
2024-10-17 09:14:55 -0400

    created text.py, added in read(), created test_text.py, and added in def test_read()

---
+++ data/tests/test_text.py
@@ -0,0 +1,11 @@
+#import pytest
+import data.text as tx
+
+def test_read():
+    text_data = tx.read()
+    assert isinstance(text_data, dict)
+    assert len(text_data) > 0
+    for _id, text in text_data.items():
+        assert isinstance(_id, str)
+        assert tx.TITLE in text
+        assert tx.TEXT in text
