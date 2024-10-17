da28cbd8bbec2248e5660b30cacc95f7c26531ef
RuchirBo <ruchirb@rocketmail.com>
2024-10-17 13:55:01 -0400

    Added SUBM_KEY to text_dict and defined read_one

--- data/text.py
+++ data/text.py
@@ -9,6 +9,7 @@ TEXT = 'text'
 EMAIL = 'email'
 
 TEST_KEY = 'HomePage'
+SUBM_KEY = 'SubmissionsPage'
 DEL_KEY = 'DeletePage'
 
 text_dict = {
@@ -16,9 +17,13 @@ text_dict = {
         TITLE: 'Home Page',
         TEXT: 'This is a journal about building API servers.',
     },
+    SUBM_KEY: {
+        TITLE: "Submissions Page",
+        TEXT: "All submissions must be original work in Word format"
+    },
     DEL_KEY: {
         TITLE: 'Home Page',
-        TEXT: 'This is a journal about building API servers.',
+        TEXT: 'This is a text to delete.',
     },
 }
 
@@ -32,6 +37,14 @@ def read():
     """
     text = text_dict
     return text
+
+def read_one(key: str) -> dict:
+    # This should take a key and return the page dictionary
+    # for that key. Return an empty dictionary if key not found.
+    result = {}
+    if key in text_dict:
+        result = text_dict[key]
+    return result
 
 
 def main():
