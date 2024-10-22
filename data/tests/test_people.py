0955734a2d84b02fe6dcc02066ec8c90f7390852
RuchirBo <ruchirb@rocketmail.com>
2024-10-22 11:55:24 -0400

    Created more invalid and valid test emails

--- data/tests/test_people.py
+++ data/tests/test_people.py
@@ -1,5 +1,26 @@
 import data.people as ppl
 
+# Invalid Test Emails:
+NO_AT = 'jkajsd'
+NO_NAME = '@kalsj'
+NO_DOMAIN = 'kajshd@'
+DOUBLE_DOT = "johnny..appleseed@forest.gov"
+FIRST_DOT = ".me@myemail"
+LAST_DOT = "me.@myemail"
+FIRST_HYPHEN = "me@-myemail"
+LAST_HYPHEN = "me@myemail-"
+DOUBLE_AT = "a@b@c@example.com"
+DOMAIN_UNDERSCORE = "i.like.underscores@but_they_are_not_allowed_here"
+# Local-Part is longer than 64 characters
+TOO_LONG_EMAIL = "1234567890123456789012345678901234567890123456789012345678901234+x@example.com"
+
+# Valid Test Emails:
+VALID_LONG = "long.email-address-with-hyphens@and.subdomains.example.com"
+SLASH_CHAR = "name/surname@example.com"
+
+
+
+
 ADD_EMAIL = "john.smith@nyu.edu"
 def test_create_person():
     ppl.create_person("John Smith", "NYU", ADD_EMAIL)
