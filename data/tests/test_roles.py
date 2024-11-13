5f6d236a684a9aae8e8f3fc5a380aa9921de7e42
RuchirBo <ruchirb@rocketmail.com>
2024-11-13 15:57:57 -0500

    Added a patch to test_create_roles

--- data/tests/test_roles.py
+++ data/tests/test_roles.py
@@ -1,5 +1,9 @@
 import data.roles as rls
+from unittest import mock
 ADD_ROLE = "RE"
+SAMPLE_ROLES = rls.get_roles()
+SAMPLE_ROLES['patched'] = 'yes'
+
 
 def test_get_masthead_roles():
     mh_roles = rls.get_masthead_roles()
@@ -14,6 +18,8 @@ def test_get_role_codes():
 
 
 def test_create_roles():
-    rls.create_roles(ADD_ROLE, "Referee")
-    codes = rls.get_role_codes()
-    assert ADD_ROLE in codes
+    with mock.patch.dict('data.roles.ROLES', SAMPLE_ROLES, clear=True):
+        rls.create_roles(ADD_ROLE, "Referee")
+        codes = rls.get_role_codes()
+        assert ADD_ROLE in codes
+        assert "patched" in codes
