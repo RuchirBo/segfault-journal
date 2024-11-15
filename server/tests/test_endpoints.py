beef6b669c86f5b55da44291e3ef78da70a951ef
Yanka Sikder <yanka@mac.myfiosgateway.com>
2024-11-14 22:56:52 -0500

    added two patch test functions for create and delete roles in test_endpoints.py

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -28,3 +28,47 @@ def test_title():
     assert ep.TITLE_RESP in resp_json
     assert isinstance(resp_json[ep.TITLE_RESP], str)
     assert len(resp_json[ep.TITLE_RESP]) > 0
+
+
+@patch(
+    "data.people.read_one",
+    return_value={
+        "name": "John Doe",
+        "roles": [],
+        "email": "test@example.com",
+    },
+)
+@patch("data.people.add_role_to_person")
+def test_add_role_success(mock_add_role, mock_read_one):
+    resp = TEST_CLIENT.post(
+        "/people/test@example.com/roles",
+        json={"role": "Editor"},
+    )
+    assert resp.status_code == 200
+    assert resp.get_json() == {
+        "message": "Role 'Editor' added to test@example.com."
+    }
+    mock_read_one.assert_called_once_with("test@example.com")
+    mock_add_role.assert_called_once_with("test@example.com", "Editor")
+
+
+@patch(
+    "data.people.read_one",
+    return_value={
+        "name": "John Doe",
+        "roles": ["Editor"],
+        "email": "test@example.com",
+    },
+)
+@patch("data.people.delete_role_from_person")
+def test_remove_role_success(mock_delete_role, mock_read_one):
+    resp = TEST_CLIENT.delete(
+        "/people/test@example.com/roles",
+        json={"role": "Editor"},
+    )
+    assert resp.status_code == 200
+    assert resp.get_json() == {
+        "message": "Role 'Editor' removed from test@example.com."
+    }
+    mock_read_one.assert_called_once_with("test@example.com")
+    mock_delete_role.assert_called_once_with("test@example.com", "Editor")
