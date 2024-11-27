c702e4be8580351c3b12d1c242f5bc835684c111
raiyahaque <raiya811@gmail.com>
2024-11-26 22:23:17 -0800

    Added tests for valid and invalid action

--- data/tests/test_query.py
+++ data/tests/test_query.py
@@ -3,6 +3,25 @@ import pytest
 import data.manuscripts.query as mqry
 
 
+def gen_random_not_valid_str() -> str:
+    BIG_NUM = 50_000_000_000
+    big_int = random.randint(0, BIG_NUM)
+    big_int += BIG_NUM
+    bad_str = str(big_int)
+
+
 def test_is_valid_state():
     for state in mqry.get_states():
         assert mqry.is_valid_state(state)
+
+
+def test_is_valid_action():
+    for action in mqry.get_actions():
+        assert mqry.is_valid_action(action)
+
+
+def test_is_invalid_action():
+    for i in range(10):
+        assert not mqry.is_valid_action(gen_random_not_valid_str())
+
+
