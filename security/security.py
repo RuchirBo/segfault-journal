b660f9e0d206a3c153733e2ad6dac683bb9dd84d
castrel17 <eac9917@nyu.edu>
2025-04-29 12:06:33 -0400

    added check_ip and check_login

--- security/security.py
+++ security/security.py
@@ -8,7 +8,8 @@ DELETE = 'delete'
 USER_LIST = 'user_list'
 CHECKS = 'checks'
 LOGIN = 'login'
-
+LOGIN_KEY = 'login_key'
+IP_ADDR = 'ip_address'
 PEOPLE = 'people'
 
 
@@ -32,3 +33,22 @@ def delete_acc(email: str):
     query = {'type': PEOPLE, 'email': email}
     db_connect.connect_db()
     return db_connect.delete_one(COLLECT_NAME, query)
+
+def is_valid_key(user_id: str, login_key: str):
+    """
+    This is just a mock of the real is_valid_key() we'll write later.
+    """
+    return True
+
+
+def check_login(user_id: str, **kwargs):
+    if LOGIN_KEY not in kwargs:
+        return False
+    return is_valid_key(user_id, kwargs[LOGIN_KEY])
+
+
+def check_ip(user_id: str, **kwargs):
+    if IP_ADDR not in kwargs:
+        return False
+    # we would check user's IP address here
+    return True
