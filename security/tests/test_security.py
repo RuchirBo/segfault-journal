d6e75af8dda84329a57f9b38eacf3ec0b9f45d5f
castrel17 <eac9917@nyu.edu>
2025-04-29 12:08:06 -0400

    added tests for check_login

--- security/tests/test_security.py
+++ security/tests/test_security.py
@@ -6,3 +6,11 @@ def test_read():
     for feature in records:
         assert isinstance(feature, str)
         assert len(feature) > 0
+
+def test_check_login_good():
+    assert security.check_login(security.GOOD_USER_ID,
+                           login_key='test_key')
+
+
+def test_check_login_bad():
+    assert not security.check_login(security.GOOD_USER_ID)
